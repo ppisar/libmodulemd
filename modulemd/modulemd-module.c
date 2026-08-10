3539f97326bb8f38227cbad2600cc77129275109
Libmodulemd CI <github-actions@github.com>
2026-08-10 15:28:55 +0000

    Code auto-formatting
    Signed-off-by: Libmodulemd CI <github-actions@github.com>

--- modulemd/modulemd-module.c
+++ modulemd/modulemd-module.c
@@ -767,18 +767,18 @@ modulemd_module_add_translation (ModulemdModule *self,
   gsize i;
   ModulemdModuleStream *stream = NULL;
   ModulemdTranslation *newtrans = NULL;
-  const gchar * self_module_name = NULL;
-  const gchar * translation_module_name = NULL;
+  const gchar *self_module_name = NULL;
+  const gchar *translation_module_name = NULL;
 
-  g_return_if_fail ( self != NULL );
-  self_module_name = modulemd_module_get_module_name(self);
-  g_return_if_fail ( self_module_name != NULL );
+  g_return_if_fail (self != NULL);
+  self_module_name = modulemd_module_get_module_name (self);
+  g_return_if_fail (self_module_name != NULL);
 
-  g_return_if_fail ( translation != NULL );
+  g_return_if_fail (translation != NULL);
   translation_module_name = modulemd_translation_get_module_name (translation);
-  g_return_if_fail ( translation_module_name != NULL );
+  g_return_if_fail (translation_module_name != NULL);
 
-  g_return_if_fail ( g_str_equal (translation_module_name, self_module_name) );
+  g_return_if_fail (g_str_equal (translation_module_name, self_module_name));
 
   newtrans = modulemd_translation_copy (translation);
 
@@ -824,18 +824,18 @@ modulemd_module_add_obsoletes (ModulemdModule *self,
   ModulemdModuleStream *stream = NULL;
   ModulemdObsoletes *new_obsoletes = NULL;
   ModulemdObsoletes *current_obsoletes = NULL;
-  const gchar * self_module_name = NULL;
-  const gchar * obsoletes_module_name = NULL;
+  const gchar *self_module_name = NULL;
+  const gchar *obsoletes_module_name = NULL;
 
-  g_return_if_fail ( self != NULL );
-  self_module_name = modulemd_module_get_module_name(self);
-  g_return_if_fail ( self_module_name != NULL );
+  g_return_if_fail (self != NULL);
+  self_module_name = modulemd_module_get_module_name (self);
+  g_return_if_fail (self_module_name != NULL);
 
-  g_return_if_fail ( obsoletes != NULL );
+  g_return_if_fail (obsoletes != NULL);
   obsoletes_module_name = modulemd_obsoletes_get_module_name (obsoletes);
-  g_return_if_fail ( obsoletes_module_name != NULL );
+  g_return_if_fail (obsoletes_module_name != NULL);
 
-  g_return_if_fail ( g_str_equal (obsoletes_module_name, self_module_name) );
+  g_return_if_fail (g_str_equal (obsoletes_module_name, self_module_name));
 
   new_obsoletes = modulemd_obsoletes_copy (obsoletes);
 
